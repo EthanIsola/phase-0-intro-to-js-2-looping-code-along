b8b067a37660e2707c3114d97cf39ef8f825d48a
Ethan Isola <isolaethan@gmail.com>
2021-12-27 23:43:32 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solutions in this file
+function writeCards(letters){
+    for (let i = 0; i < letters.length; i++) {
+        console.log(letters[i]=`Thank you, ${letters[i]}, for the wonderful surprise gift!`);
+    }
+    return letters;
+}
+function countDown(n){
+    while (n>=0){
+        console.log(n);
+        n--;
+    }
+}
